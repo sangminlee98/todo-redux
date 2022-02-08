66119316b2a88c9256b5170c4b60801b79754bd4
sangminlee98 <tkdals0975@naver.com>
2022-02-08 17:53:29 +0900

    add redux-devtool

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,9 @@ import App from './App';
 import rootReducer from './modules';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
