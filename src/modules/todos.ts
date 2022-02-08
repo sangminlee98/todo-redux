ef227277204538b274ca29922f52e8309aaf6112
sangminlee98 <tkdals0975@naver.com>
2022-02-08 17:52:15 +0900

    add counter & todos modules

---
+++ src/modules/todos.ts
@@ -0,0 +1,81 @@
+type Type = 'todos/CHANGE_INPUT' | 'todos/INSERT' | 'todos/TOGGLE' | 'todos/REMOVE';
+type Todo = {
+  id: number,
+  text: string,
+  done: boolean
+}
+type State = {
+  input: any,
+  todos: Todo[]
+}
+type Action = {type: Type, input?: any, todo?: Todo, id?: number};
+const CHANGE_INPUT: Type = 'todos/CHANGE_INPUT'; 
+const INSERT: Type = 'todos/INSERT';
+const TOGGLE: Type = 'todos/TOGGLE';
+const REMOVE: Type = 'todos/REMOVE';
+
+export const changeInput = (input: any) => ({
+  type: CHANGE_INPUT,
+  input
+});
+let id = 3;
+export const insert = (text: string) => ({
+  type: INSERT,
+  todo: {
+    id: id++,
+    text,
+    done: false
+  }
+});
+
+export const toggle = (id: number) => ({
+  type: TOGGLE,
+  id
+})
+export const remove = (id: number) => ({
+  type: REMOVE,
+  id
+});
+
+const initialState: State = {
+  input: '',
+  todos: [
+    {
+      id: 1,
+      text: '리덕스 기초 배우기',
+      done: true
+    },
+    {
+      id: 2,
+      text: '리액트와 리덕스 사용하기',
+      done: false
+    },
+  ]
+};
+
+const todos = (state = initialState, action: Action) => {
+  switch(action.type) {
+    case CHANGE_INPUT:
+      return {
+        ...state,
+        input: action.input
+      }
+    case INSERT:
+      return {
+        ...state,
+        todos: state.todos.concat(action.todo!)
+      }
+    case TOGGLE:
+      return {
+        ...state,
+        todos: state.todos.map(todo =>
+          todo.id === action.id ? {...todo, done: !todo.done} : todo)
+      };
+    case REMOVE:
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo.id !== action.id)
+      }
+  }
+}
+export default todos;
